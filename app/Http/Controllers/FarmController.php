160220fec3bf7963e2ba6940e8de955549acaeff
Your Name <you@example.com>
2023-05-22 20:31:44 +0700

    create controllers

--- app/Http/Controllers/FarmController.php
+++ app/Http/Controllers/FarmController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class RoleController extends Controller
+class FarmController extends Controller
 {
     /**
      * Display a listing of the resource.
